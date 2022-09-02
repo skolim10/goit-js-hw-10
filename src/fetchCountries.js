df0909bdc8363a254aa09f9becc23e5856fad8a1
Tomasz Skolimo <skolim10@gmail.com>
2022-09-02 14:13:37 +0200

    ver1

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+function techCountries(name) {
+  return fetch(
+    'https://restcountries.com/v3.1/name/{name}?fields=name,capital,population,flags,languages'
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+//=====FUNCTIONS=====
+
+const cleanData = () => {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+};
+
+const countrySearch = e => {
+  const countryFind = e.target.value.trim();
+  if (!countryFind) {
+    cleanData();
+    return;
+  }
+  fetchCountries(countryFind)
+    .then(country => {
+      if (country.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        cleanData();
+        return;
+      } else if (country.length > 1 && country.length <= 10) {
+        cleanData(countryInfo.innerHTML);
+        renderCountryList(country);
+      } else if (country.length === 1) {
+        cleanData(countryList.innerHTML);
+        renderCountryInfo(country);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      cleanData();
+      return error;
+    });
+};
+
+const renderCountryList = country => {
+  const markup = country
+    .map(({ name, flags }) => {
+      return `<li><img src="${flags.svg}" alt="${name.official}" width="100" height="60"><p>${name.official}</p></li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+};
+
+const renderCountryInfo = country => {
+  const markup = country
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<section><h1><img src="${flags.svg}" alt="${
+        name.official
+      }" width="100" height="60">&nbsp ${name.official}</h1>
+      <p><span>Capital: </span>&nbsp ${capital}</p>
+      <p><span>Population:</span>&nbsp ${population}</p>
+      <p><span>Languages:</span>&nbsp ${Object.values(languages)}</p><section>`;
+    })
+    .join('');
+  countryInfo.innerHTML = markup;
+};
+
+input.addEventListener('input', debounce(countrySearch, DEBOUNCE_DELAY));
